1bbde3cf9ef3fabde5f92e3f2023169a0bee805a
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-04-09 06:26:52 +0000

    [Finish] Application task6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,16 +3,24 @@ class BookCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    if comment.save
-      redirect_back(fallback_location: root_path)
-    else
-      redirect_back(fallback_location: root_path)
-    end
+    comment.save
+    @book_comment = BookComment.new
+    @book = Book.find(params[:book_id])
+    # if comment.save
+    #   # redirect_back(fallback_location: root_path)
+    # else
+    #   # redirect_back(fallback_location: root_path)
+    # end
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment=BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @book_comment.destroy
+    
+    @book_comment = BookComment.new
+    # redirect_back(fallback_location: root_path)
+
   end
 
 
