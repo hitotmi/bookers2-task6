cb059683f96b4db436210ad22aba2def18f96a97
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-05-16 11:41:59 +0000

    [Add]画面のトップにスクロール

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -21,3 +21,12 @@ $(document).on('turbolinks:load', function () {
     pauseOnDotsHover: false,
   });
 });
+
+$(document).on('turbolinks:load', function () {
+  $('#page-top').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+  });
+});
