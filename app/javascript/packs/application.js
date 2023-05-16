aa30d46af6994114fc11ee31b85f470caaa1bd96
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-05-16 10:30:24 +0000

    JavaScriptを使ってアラート表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all';
+import 'jquery'
+import "script.js"
 
 
 Rails.start()
